5cf6fcd2685712eed17c1fbb81baec3a0c801271
KarthikUdayMurthy <u.karthik99@gmail.com>
2021-09-14 19:08:36 +0530

    Moon Phase fine tuning

--- src/Util/Components/MoonPhase.tsx
+++ src/Util/Components/MoonPhase.tsx
@@ -8,7 +8,14 @@ const MoonPhase: React.FC<MoonPhaseProps> = ({ value }) => {
   let phaseVal: number = value * 2;
   phaseVal = phaseVal > 1 ? phaseVal - 1 : phaseVal;
   const size = 25;
-  const fctr = size * 0.75 * phaseVal;
+  let fctr = 0;
+  if (phaseVal < 0.75) {
+    fctr = size * 0.5 * phaseVal;
+  } else if (phaseVal >= 0.75 && phaseVal < 0.9) {
+    fctr = size * 0.66 * phaseVal;
+  } else {
+    fctr = size * 0.75 * phaseVal;
+  }
   const moonStyle = {
     boxShadow: `${fctr}px ${fctr * -1}px 0 0 white inset`
   };
